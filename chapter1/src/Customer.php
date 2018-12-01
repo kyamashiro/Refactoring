adfd374c2773ebf3fbcf0e49a64a1f794045569f
kyamashiro <yamashiro@rrr-z.co.jp>
2018-12-01 23:40:15 +0900

    divide method

--- chapter1/src/Customer.php
+++ chapter1/src/Customer.php
@@ -44,28 +44,10 @@ class Customer
         $totalAmount = 0;
         $frequentRenterPoints = 0;
         $rentals = $this->rentals;
-        $result = nl2br("Rental Record for {$this->getName()}\n");
+        $result = "Rental Record for {$this->getName()}\n";
 
         foreach ($rentals as $each) {
-            $thisAmount = 0;
-
-            switch ($each->getMovie()->getPriceCode()) {
-                case Movie::REGULAR:
-                    $thisAmount += 2;
-                    if ($each->getDaysRented() > 2) {
-                        $thisAmount += ($each->getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie::NEW_RELEASE:
-                    $thisAmount += $each->getDaysRented() * 3;
-                    break;
-                case Movie::CHILDRENS:
-                    $thisAmount += 1.5;
-                    if ($each->getDaysRented() > 3) {
-                        $thisAmount += ($each->getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+            $thisAmount = $this->amountFor($each);
 
             $frequentRenterPoints++;
             if (($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE)
@@ -73,12 +55,40 @@ class Customer
                 $frequentRenterPoints++;
             }
 
-            $result .= nl2br("{$each->getMovie()->getTitle()} {$thisAmount}\n");
+            $result .= "{$each->getMovie()->getTitle()} {$thisAmount}\n";
             $totalAmount += $thisAmount;
         }
 
-        $result .= nl2br("Amount owed is {$totalAmount}\n");
-        $result .= nl2br("You earned {$frequentRenterPoints} frequent renter points \n");
+        $result .= "Amount owed is {$totalAmount}\n";
+        $result .= "You earned {$frequentRenterPoints} frequent renter points \n";
         return $result;
+    }
+
+    /**
+     * @param $each
+     * @return float|int
+     */
+    private function amountFor(Rental $each): float
+    {
+        $thisAmount = 0;
+
+        switch ($each->getMovie()->getPriceCode()) {
+            case Movie::REGULAR:
+                $thisAmount += 2;
+                if ($each->getDaysRented() > 2) {
+                    $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie::NEW_RELEASE:
+                $thisAmount += $each->getDaysRented() * 3;
+                break;
+            case Movie::CHILDRENS:
+                $thisAmount += 1.5;
+                if ($each->getDaysRented() > 3) {
+                    $thisAmount += ($each->getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return $thisAmount;
     }
 }
